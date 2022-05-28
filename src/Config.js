530fd3b182bfeaff07691d2a70ba6edef491ad86
Jaden <56017866+jaden-wei@users.noreply.github.com>
2022-05-28 13:37:57 -0700

    Delete Config.js

--- src/Config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyBYXqQcxS9zT0npZFjg_xqUMHcy4JEgpTw",
-  authDomain: "blog-81191.firebaseapp.com",
-  projectId: "blog-81191",
-  storageBucket: "blog-81191.appspot.com",
-  messagingSenderId: "923824322783",
-  appId: "1:923824322783:web:e345b42608fe298e95916a",
-};
